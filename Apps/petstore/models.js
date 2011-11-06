660b98daa5c8288514197ea774033549b8281d89
semu <c@semu.mp>
2011-11-06 11:31:59 +0000

    Added old petstore. Starting to port petstore to new binding schema

---
+++ Apps/petstore/models.js
@@ -0,0 +1,35 @@
+/**
+ * this is a JSON schema for the PetModel
+ */
+exports.pet = {
+  "properties" : {
+    "tags" : {
+      "type" : "array",
+      "items" : {
+        "$ref" : "tag"
+      }
+    },
+    "id" : {
+      "type" : "long"
+    },
+    "category" : {
+      "type" : "category"
+    },
+    "status" : {
+      "type" : "string",
+      "description" : "pet status in the store",
+      "enum" : [ "available", "pending", "sold" ]
+    },
+    "name" : {
+      "type" : "string"
+    },
+    "photoUrls" : {
+      "type" : "array",
+      "items" : {
+        "type" : "string"
+      }
+    }
+  },
+  "id" : "pet"
+};
+
