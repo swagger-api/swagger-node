152ad2bd13acc8b4d4a096210b8c5b7833423c28
Tony Tam <fehguy@gmail.com>
2013-01-03 14:10:06 -0800

    updated filter

--- Apps/petstore/main.js
+++ Apps/petstore/main.js
@@ -28,13 +28,13 @@ app.use(express.bodyParser());
 // Set the main handler in swagger to the express app
 swagger.setAppHandler(app);
 
-// This is a sample validator.  It simply says that for _all_ POST
+// This is a sample validator.  It simply says that for _all_ POST, DELETE, PUT
 // methods, the header `api_key` OR query param `api_key` must be equal
 // to the string literal `special-key`.  All other HTTP ops are A-OK
 swagger.addValidator(
   function validate(req, path, httpMethod) {
     //  example, only allow POST for api_key="special-key"
-    if ("POST" == httpMethod) {
+    if ("POST" == httpMethod || "DELETE" == httpMethod || "PUT" == httpMethod) {
       var apiKey = req.headers["api_key"];
       if (!apiKey) {
         apiKey = url.parse(req.url,true).query["api_key"]; }
