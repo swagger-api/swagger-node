6e1242c269f13424371b62f69d7852017a65636f
Chris Katz <chris.katz@wonderlic.com>
2014-07-15 13:41:55 -0500

    created rest of methods and wired up to swagger as well as writing unit tests and fixing code based off them.

--- test/validation.tests.js
+++ test/validation.tests.js
@@ -8,45 +8,55 @@ describe('validation', function() {
 
   describe('float', function() {
     it('should validate', function() {
-      var ret = validation.validateFloatParameter(1.5, 'testParam');
+      var ret = validation.validateFloatParameter(1.5, { required: false, name: 'testParam'});
       (ret === null).should.be.true;
     });
 
     it('should validate with large number', function() {
-      var ret = validation.validateFloatParameter(112312234132443.88635, 'testParam');
+      var ret = validation.validateFloatParameter(112312234132443.88635,  { required: false, name: 'testParam'});
       (ret === null).should.be.true;
     });
 
-    it('should not validate with negative too large number', function() {
-      var ret = validation.validateFloatParameter(-112312234132443.88635, 'testParam');
+    it('should validate with hex', function() {
+      var ret = validation.validateFloatParameter(0x123,  { required: false, name: 'testParam'});
+      (ret === null).should.be.true;
+    });
+
+    it('should validate with hex as string', function() {
+      var ret = validation.validateFloatParameter("0x123",  { required: false, name: 'testParam'});
       (ret === null).should.be.true;
     });
 
     it('should validate with decimal of 0', function() {
-      var ret = validation.validateFloatParameter(1.0, 'testParam');
+      var ret = validation.validateFloatParameter(1.0,  { required: false, name: 'testParam'});
       (ret === null).should.be.true;
     });
 
     it('should not validate with too large number', function() {
-      var ret = validation.validateFloatParameter(1e4500, 'testParam');
+      var ret = validation.validateFloatParameter(1e4500,  { required: false, name: 'testParam'});
       (ret === null).should.be.false;
       ret.should.equal("Parameter testParam is not a type of float");
     });
 
     it('should not validate with negative too large number', function() {
-      var ret = validation.validateFloatParameter(-1e4500, 'testParam');
+      var ret = validation.validateFloatParameter(-112312234132443.88635,  { required: false, name: 'testParam'});
+      (ret === null).should.be.true;
+    });
+
+    it('should not validate with negative too large number', function() {
+      var ret = validation.validateFloatParameter(-1e4500,  { required: false, name: 'testParam'});
       (ret === null).should.be.false;
       ret.should.equal("Parameter testParam is not a type of float");
     });
 
     it('should not validate with object', function() {
-      var ret = validation.validateFloatParameter({}, 'testParam');
+      var ret = validation.validateFloatParameter({},  { required: false, name: 'testParam'});
       (ret === null).should.be.false;
       ret.should.equal("Parameter testParam is not a type of float");
     });
 
     it('should not validate with string', function() {
-      var ret = validation.validateFloatParameter('string', 'testParam');
+      var ret = validation.validateFloatParameter('string',  { required: false, name: 'testParam'});
       (ret === null).should.be.false;
       ret.should.equal("Parameter testParam is not a type of float");
     });
@@ -54,40 +64,60 @@ describe('validation', function() {
 
   describe('integer', function() {
     it('should validate', function() {
-      var ret = validation.validateIntegerParameter(1, 'testParam');
+      var ret = validation.validateIntegerParameter(1,  { required: false, name: 'testParam'});
+      (ret === null).should.be.true;
+    });
+
+    it('should validate with number as string', function() {
+      var ret = validation.validateIntegerParameter('1',  { required: false, name: 'testParam'});
       (ret === null).should.be.true;
     });
 
     it('should validate with large number', function() {
-      var ret = validation.validateIntegerParameter(123124582, 'testParam');
+      var ret = validation.validateIntegerParameter(123124582,  { required: false, name: 'testParam'});
+      (ret === null).should.be.true;
+    });
+
+    it('should validate with hex', function() {
+      var ret = validation.validateIntegerParameter(0x123,  { required: false, name: 'testParam'});
+      (ret === null).should.be.true;
+    });
+
+    it('should validate with hex as string', function() {
+      var ret = validation.validateIntegerParameter("0x123",  { required: false, name: 'testParam'});
       (ret === null).should.be.true;
     });
 
     it('should validate float with no decimal', function() {
-      var ret = validation.validateIntegerParameter(1.0, 'testParam');
+      var ret = validation.validateIntegerParameter(1.0,  { required: false, name: 'testParam'});
+      (ret === null).should.be.true;
+    });
+
+    it('should validate float with notation', function() {
+      var ret = validation.validateIntegerParameter(2e0,  { required: false, name: 'testParam'});
       (ret === null).should.be.true;
     });
 
     it('should not validate with too large number', function() {
-      var ret = validation.validateIntegerParameter(5656423372324422244, 'testParam');
+      var ret = validation.validateIntegerParameter(5656423372324422244,  { required: false, name: 'testParam'});
       (ret === null).should.be.false;
       ret.should.equal("Parameter testParam is not a type of integer");
     });
 
     it('should not validate with float', function() {
-      var ret = validation.validateIntegerParameter(1.2, 'testParam');
+      var ret = validation.validateIntegerParameter(1.2,  { required: false, name: 'testParam'});
       (ret === null).should.be.false;
       ret.should.equal("Parameter testParam is not a type of integer");
     });
 
     it('should not validate with object', function() {
-      var ret = validation.validateIntegerParameter({}, 'testParam');
+      var ret = validation.validateIntegerParameter({},  { required: false, name: 'testParam'});
       (ret === null).should.be.false;
       ret.should.equal("Parameter testParam is not a type of integer");
     });
 
     it('should not validate with string', function() {
-      var ret = validation.validateIntegerParameter('string', 'testParam');
+      var ret = validation.validateIntegerParameter('string',  { required: false, name: 'testParam'});
       (ret === null).should.be.false;
       ret.should.equal("Parameter testParam is not a type of integer");
     });
@@ -95,59 +125,81 @@ describe('validation', function() {
 
   describe('string', function() {
     it('should validate', function() {
-      var ret = validation.validateStringParameter('Hi', 'testParam');
+      var ret = validation.validateStringParameter('Hi',  { required: false, name: 'testParam'});
       (ret === null).should.be.true;
     });
 
     it('should validate with large string', function() {
-      var ret = validation.validateStringParameter('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'testParam');
+      var ret = validation.validateStringParameter('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',  { required: false, name: 'testParam'});
       (ret === null).should.be.true;
     });
 
     it('should not validate with number', function() {
-      var ret = validation.validateStringParameter(1, 'testParam');
+      var ret = validation.validateStringParameter(1,  { required: false, name: 'testParam'});
       (ret === null).should.be.false;
       ret.should.equal("Parameter testParam is not a type of string");
     });
 
     it('should not validate with boolean', function() {
-      var ret = validation.validateStringParameter(true, 'testParam');
+      var ret = validation.validateStringParameter(true,  { required: false, name: 'testParam'});
       (ret === null).should.be.false;
       ret.should.equal("Parameter testParam is not a type of string");
     });
 
     it('should not validate with object', function() {
-      var ret = validation.validateStringParameter({}, 'testParam');
+      var ret = validation.validateStringParameter({},  { required: false, name: 'testParam'});
       (ret === null).should.be.false;
       ret.should.equal("Parameter testParam is not a type of string");
     });
   });
 
   describe('boolean', function() {
-    it('should validate', function() {
-      var ret = validation.validateBoolParameter(true, 'testParam');
+    it('should validate with true', function() {
+      var ret = validation.validateBoolParameter(true,  { required: false, name: 'testParam'});
       (ret === null).should.be.true;
     });
 
-    it('should validate with large string', function() {
-      var ret = validation.validateBoolParameter(false, 'testParam');
+    it('should validate with false', function() {
+      var ret = validation.validateBoolParameter(false,  { required: false, name: 'testParam'});
+      (ret === null).should.be.true;
+    });
+
+    it('should validate with true string', function() {
+      var ret = validation.validateBoolParameter('true',  { required: false, name: 'testParam'});
+      (ret === null).should.be.true;
+    });
+
+    it('should validate with false string', function() {
+      var ret = validation.validateBoolParameter('false',  { required: false, name: 'testParam'});
       (ret === null).should.be.true;
     });
 
     it('should not validate with number', function() {
-      var ret = validation.validateBoolParameter(1, 'testParam');
+      var ret = validation.validateBoolParameter(1,  { required: false, name: 'testParam'});
+      (ret === null).should.be.false;
+      ret.should.equal("Parameter testParam is not a type of boolean");
+    });
+
+    it('should not validate with True string', function() {
+      var ret = validation.validateBoolParameter('True',  { required: false, name: 'testParam'});
+      (ret === null).should.be.false;
+      ret.should.equal("Parameter testParam is not a type of boolean");
+    });
+
+    it('should not validate with False string', function() {
+      var ret = validation.validateBoolParameter('False',  { required: false, name: 'testParam'});
       (ret === null).should.be.false;
       ret.should.equal("Parameter testParam is not a type of boolean");
     });
 
     it('should not validate with string', function() {
-      var ret = validation.validateBoolParameter('Hello World', 'testParam');
+      var ret = validation.validateBoolParameter('Hello World',  { required: false, name: 'testParam'});
       (ret === null).should.be.false;
       ret.should.equal("Parameter testParam is not a type of boolean");
     });
 
     it('should not validate with object', function() {
-      var ret = validation.validateBoolParameter({}, 'testParam');
+      var ret = validation.validateBoolParameter({},  { required: false, name: 'testParam'});
       (ret === null).should.be.false;
       ret.should.equal("Parameter testParam is not a type of boolean");
     });
