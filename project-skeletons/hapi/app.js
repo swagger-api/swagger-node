ef8004debbbcb5013dd834e4b27c5e87fbe2f1a1
Scott Ganyo <scott@ganyo.com>
2015-04-04 12:03:20 -0700

    Hapi support

---
+++ project-skeletons/hapi/app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var SwaggerRunner = require('swagger-node-runner');
+var Hapi = require('hapi');
+var app = new Hapi.Server();
+
+module.exports = app; // for testing
+
+var config = {
+  appRoot: __dirname // required config
+};
+
+SwaggerRunner.create(config, function(err, runner) {
+  if (err) { throw err; }
+
+  var port = process.env.PORT || 10010;
+  app.connection({ port: port });
+
+  app.register(runner.hapiMiddleware().plugin, function(err) {
+    if (err) {
+      console.error('Failed to load plugin:', err);
+    }
+  });
+
+  app.start(function() {
+    console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
+  });
+});
