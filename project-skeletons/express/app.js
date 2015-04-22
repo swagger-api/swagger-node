7c4b8da6a9ebea4ebcceebc068c53d1dd08fe3ed
Scott Ganyo <scott@ganyo.com>
2015-04-21 20:11:49 -0700

    fix typo in express skeleton

--- project-skeletons/express/app.js
+++ project-skeletons/express/app.js
@@ -13,7 +13,7 @@ SwaggerRunner.create(config, function(err, runner) {
 
   // install swagger-node-runner middleware
   var expressMiddleware = runner.expressMiddleware();
-  app.use(connectMiddleware.chain());
+  app.use(expressMiddleware.chain());
 
   var port = process.env.PORT || 10010;
   app.listen(port);
