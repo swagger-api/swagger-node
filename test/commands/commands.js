a11f64ed8e092bc70c9b3fb907c0b4ccf83714d9
Noah Dietz <noahdietz@ITs-MacBook-Air-7.local>
2015-07-31 15:09:26 -0700

    Code refactored and passes tests

--- test/commands/commands.js
+++ test/commands/commands.js
@@ -23,21 +23,39 @@ var sinon = require('sinon');
 describe('commands', function() {
   var commands;
   var converterSpy = sinon.spy();
-  var specSpy = sinon.spy();
+  var specSpy = sinon.stub().callsArg(2);
+
+  var specMock = {
+    validateSwagger: specSpy
+  };
+
+  var fsMock = {
+    existsSync: sinon.stub().returns(true),
+    readFileSync: sinon.stub().returns("{\"help\": true, \"test\": 4}")
+  }
+  var yamlMock = {
+    safeDump: sinon.stub().returns('')
+  }
 
   before(function() {
     commands = rewire('../../lib/commands/commands.js');
     commands.__set__('swaggerConverter', converterSpy);
+    commands.__set__('fs', fsMock);
+    commands.__set__('yaml', yamlMock);
+
+    commands.__set__('swaggerSpec', specMock);
   });
 
-  xit ('should run validate with no issues', function() {
+  it ('should run validate with no issues', function(done) {
     commands.validate('test.yaml', {}, function(err) {
-      specSpy.called.should.be.true;
+      expect(specSpy.called).to.be.true;
+      done(err);
     });
   });
 
   it ('should run convert with no issues', function(done) {
     commands.convert('test.yaml', [], {}, function (error) {
+      expect(converterSpy.called).to.be.true;
       done(error);
     });
   });
