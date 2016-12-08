0cb1f34a639bbcfed283693a8c453ed38206c68b
Henning von See <henning@seewes.de>
2016-12-08 19:22:13 +0100

    Increased timeout to avoid error in travis

--- test/commands/project/project.js
+++ test/commands/project/project.js
@@ -490,7 +490,7 @@ describe('project', function() {
       setTimeout(function () {
         stdin.send('n\n');
         stdin.send('n\n');
-      }, 250);
+      }, 5000);
 
       project.generateTest(projPath, {}, function(err) {
         fs.existsSync(path.resolve(projPath, 'test/api/client/hello-test.js')).should.be.ok;
