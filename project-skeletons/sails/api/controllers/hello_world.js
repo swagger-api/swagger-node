7593575663adcbdafd2cbf3c4c3bb1b9ff2391b7
meisterlampe <meisterlampe@users.noreply.github.com>
2017-02-20 11:58:51 +0100

    Update hello_world.js

--- project-skeletons/sails/api/controllers/hello_world.js
+++ project-skeletons/sails/api/controllers/hello_world.js
@@ -37,8 +37,8 @@ module.exports = {
 function hello(req, res) {
   // variables defined in the Swagger document can be referenced using req.swagger.params.{parameter_name}
   var name = req.swagger.params.name.value || 'stranger';
-  var hello = util.format('{ "message": "Hello, %s!" }', name);
+  var hello = util.format('Hello, %s!', name);
 
   // this sends back a JSON response which is a single string
-  res.json(hello);
+  res.json({ "message": hello });
 }
