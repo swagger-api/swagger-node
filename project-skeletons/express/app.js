e2d491d55786bbd2b81c7cb9c14f4a0a17c669f1
Michael Mrowetz <me@michaelmrowetz.com>
2017-04-15 18:27:51 +0900

    fix tests - there was a race condition with connect based tests & revert hapi to 9.5.1

--- project-skeletons/express/app.js
+++ project-skeletons/express/app.js
@@ -2,22 +2,25 @@
 
 var SwaggerExpress = require('swagger-express-mw');
 var app = require('express')();
-module.exports = app; // for testing
+// export setup Promis for testing
+module.exports = new Promise(function (resolve, reject) {
 
-var config = {
-  appRoot: __dirname // required config
-};
+  var config = {
+    appRoot: __dirname // required config
+  };
 
-SwaggerExpress.create(config, function(err, swaggerExpress) {
-  if (err) { throw err; }
+  SwaggerExpress.create(config, function (err, swaggerExpress) {
+    if (err) { throw err; }
 
-  // install middleware
-  swaggerExpress.register(app);
+    // install middleware
+    swaggerExpress.register(app);
 
-  var port = process.env.PORT || 10010;
-  app.listen(port);
-
-  if (swaggerExpress.runner.swagger.paths['/hello']) {
-    console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
-  }
+    var port = process.env.PORT || 10010;
+    app.listen(port, function() {
+      if (swaggerExpress.runner.swagger.paths['/hello']) {
+        console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
+      }
+      resolve(app);
+    });
+  });
 });
--- project-skeletons/hapi/app.js
+++ project-skeletons/hapi/app.js
@@ -4,27 +4,33 @@ var SwaggerHapi = require('swagger-hapi');
 var Hapi = require('hapi');
 var app = new Hapi.Server();
 
-module.exports = app; // for testing
+// export setup Promis for testing
+module.exports = new Promise(function (resolve, reject) {
 
-var config = {
-  appRoot: __dirname // required config
-};
-
-SwaggerHapi.create(config, function(err, swaggerHapi) {
-  if (err) { throw err; }
-
-  var port = process.env.PORT || 10010;
-  app.connection({ port: port });
-  app.address = function() {
-    return { port: port };
+  var config = {
+    appRoot: __dirname // required config
   };
 
-  app.register(swaggerHapi.plugin, function(err) {
-    if (err) { return console.error('Failed to load plugin:', err); }
-    app.start(function() {
-      if (swaggerHapi.runner.swagger.paths['/hello']) {
-        console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
+  SwaggerHapi.create(config, function (err, swaggerHapi) {
+    if (err) { throw err; }
+
+    var port = process.env.PORT || 10010;
+    app.connection({ port: port });
+    app.address = function () {
+      return { port: port };
+    };
+
+    app.register(swaggerHapi.plugin, function (err) {
+      if (err) {
+        console.error('Failed to load plugin:', err);
+        reject(err);
       }
+      app.start(function () {
+        if (swaggerHapi.runner.swagger.paths['/hello']) {
+          console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
+        }
+        resolve(app);
+      });
     });
   });
 });
